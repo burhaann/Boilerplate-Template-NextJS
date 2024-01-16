f89ec7c3a29db410049709c7b92ebd2bd909aa14
Burhaann <burhaann321@gmail.com>
2024-01-16 16:22:35 +0530

    first commit

---
+++ src/app/layout.js
@@ -0,0 +1,23 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Footer from "@/components/footer/Footer";
+import Navbar from "@/components/navbar/Navbar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Fullstack Blog",
+  description: "Fullstack Blog",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
